ce16fc2b303c2dfa1032976c91cf1d258950e926
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-06-14 11:06:32 +0800

    Update openNews.js

--- js/openNews.js
+++ js/openNews.js
@@ -1,16 +1,35 @@
 if (!localStorage.isClickedNews) {
-    layui.use('layer', function() {
-        layui.layer.open({
-            type: 2,
-            title: 'Map-Flight新闻 :)',
-            shadeClose: true,
-            shade: false,
-            maxmin: true,
-            area: ['693px', '400px'],
-            content: 'news/news.html',
-            end: function() {
-                localStorage.isClickedNews = true;
-            }
+    if (/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent)) || /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        //手机访问
+        layui.use('layer', function() {
+            layui.layer.open({
+                type: 2,
+                title: 'Map-Flight新闻 :)',
+                shadeClose: true,
+                shade: false,
+                maxmin: true,
+                area: ['100%', '400px'],
+                content: 'news/news.html',
+                end: function() {
+                    localStorage.isClickedNews = true;
+                }
+            })
         })
-    })
+    } else {
+        //电脑访问
+        layui.use('layer', function() {
+            layui.layer.open({
+                type: 2,
+                title: 'Map-Flight新闻 :)',
+                shadeClose: true,
+                shade: false,
+                maxmin: true,
+                area: ['693px', '400px'],
+                content: 'news/news.html',
+                end: function() {
+                    localStorage.isClickedNews = true;
+                }
+            })
+        })
+    }
 }
