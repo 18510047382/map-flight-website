dfd8f71422dbf0363d5a424773931af220cceec1
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-06-21 10:34:09 +0800

    Update closeTopNavRightBtn.js

--- js/closeTopNavRightBtn.js
+++ js/closeTopNavRightBtn.js
@@ -1,3 +1,5 @@
-var evt = document.createEvent('HTMLEvents');
-evt.initEvent('mouseout', true, true);
-document.querySelector('#top-nav-me').dispatchEvent(evt);
+document.querySelector('#top-nav-me-close-btn').onclick = function() {
+    var evt = document.createEvent('HTMLEvents');
+    evt.initEvent('mouseout', true, true);
+    document.querySelector('#top-nav-me').dispatchEvent(evt);
+}
