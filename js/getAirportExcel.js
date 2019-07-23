9cf0b5a1b7d05d7410b6f34dc744d7681ca7015d
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-07-23 21:15:41 +0800

    Update getAirportExcel.js

--- js/getAirportExcel.js
+++ js/getAirportExcel.js
@@ -22,7 +22,7 @@ function getAirportExcelFn() {
                 this.info.isOpen = false;
                 return;
             }
-            this.openInfoWindow(new BMap.InfoWindow('<h3><b>机场信息：</b></h3><p>名称：' + this.info.name + '</p><p>IACO代码：' + this.info.code + '</p><p>经度：' + this.info.lon + '</p><p>纬度：' + this.info.lat + '</p><p>海拔：' + this.info.ft + 'ft</p>'));
+            this.openInfoWindow(new BMap.InfoWindow('<h3 style="color: #1E9FFF"><b>机场信息：</b></h3><p>名称：' + this.info.name + '</p><p>IACO代码：' + this.info.code + '</p><p>经度：' + this.info.lon + '</p><p>纬度：' + this.info.lat + '</p><p>海拔：' + this.info.ft + 'ft</p>'));
             this.info.isOpen = true;
         }
 
