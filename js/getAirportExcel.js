befad831e8f0d63b37492e0e995b24bd0ee70d0f
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-07-24 12:36:49 +0800

    Update getAirportExcel.js

--- js/getAirportExcel.js
+++ js/getAirportExcel.js
@@ -1,3 +1,6 @@
+var componentAirport = document.querySelector('#component-airport'),
+    componentAirportCloseBar = document.querySelector('#component-airport-closeBar');
+
 function getAirportExcelFn() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", 'data/airport.csv', false);
@@ -17,13 +20,83 @@ function getAirportExcelFn() {
             icon: airportIcon
         })
         mk.onclick = function() {
-            if (this.info.isOpen) {
-                this.closeInfoWindow();
-                this.info.isOpen = false;
-                return;
+            document.querySelector('#component-airport-index-airportName').innerText = this.info.name;
+            document.querySelector('#component-airport-index-airportCode').innerText = this.info.code;
+            document.querySelector('#component-airport-index-info-locationLon').innerText = this.info.lon;
+            document.querySelector('#component-airport-index-info-locationLat').innerText = this.info.lat;
+            document.querySelector('#component-airport-index-info-attitudeFt').innerText = parseInt(this.info.ft).toString() + 'ft';
+            document.querySelector('#component-airport-index-info-attitudeMeter').innerText = (parseInt(this.info.ft) * 0.3048) + 'm';
+            var nowServerDetail;
+            switch (nowServer) {
+                case 'cs':
+                    nowServerDetail = 'Casual Server';
+                    break;
+                case 'ts':
+                    nowServerDetail = 'Training Server';
+                    break;
+                case 'es':
+                    nowServerDetail = 'Expert Server';
+                    break;
             }
-            this.openInfoWindow(new BMap.InfoWindow('<h3 style="color: #1E9FFF"><b>机场信息：</b></h3><p>名称：' + this.info.name + '</p><p>IACO代码：' + this.info.code + '</p><p>经度：' + this.info.lon + '</p><p>纬度：' + this.info.lat + '</p><p>海拔：' + this.info.ft + 'ft</p>'));
-            this.info.isOpen = true;
+            if (typeof atc[nowServerDetail][this.info.code] === 'undefined') {
+                document.querySelector('#component-airport-info-error').style.display = 'block';
+                document.querySelector('#component-airport-info-ok').style.display = 'none';
+            } else {
+                //渲染atc界面
+                var html = '';
+                for (var i in atc[nowServerDetail][this.info.code]) {
+                    html += '<span tabindex="0" type="button" id="component-airport-info-ok-1span"><div><span style="height: 100%; width: 100%; position: absolute; top: 0px; left: 0px; overflow: hidden;"></span><div id="component-airport-info-ok-1span-div"><svg viewBox="0 0 24 24" style="display: block; fill: rgb(117, 117, 117); height: 24px; width: 24px; user-select: none; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; position: absolute; top: 0px; margin: 12px; color: rgb(117, 117, 117); left: 4px;"><path d="M12 1c-4.97 0-9 4.03-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h4v1h-7v2h6c1.66 0 3-1.34 3-3V10c0-4.97-4.03-9-9-9z"></path></svg><button id="component-airport-info-ok-1span-div-btn" tabindex="0" type="button"><div><svg viewBox="0 0 24 24" style="display: inline-block; fill: rgba(0, 0, 0, 0.87); height: 24px; width: 24px; user-select: none; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg></div></button><div>';
+                    switch (i) {
+                        case '0':
+                            html += 'Ground'
+                            break;
+                        case '1':
+                            html += 'Tower'
+                            break;
+                        case '2':
+                            html += 'Unicom'
+                            break;
+                        case '3':
+                            html += 'Clearance'
+                            break;
+                        case '4':
+                            html += 'Approach'
+                            break;
+                        case '5':
+                            html += 'Departure'
+                            break;
+                        case '6':
+                            html += 'Center'
+                            break;
+                        case '7':
+                            html += 'ATIS'
+                            break;
+                        case '8':
+                            html += 'Aircraft'
+                            break;
+                        case '9':
+                            html += 'Recorded'
+                            break;
+                        case '10':
+                            html += 'Unknown'
+                            break;
+                        case '11':
+                            html += 'Unused'
+                            break;
+                    }
+                    html += '</div></div></div></span><div style="padding: 16px; font-weight: 500; box-sizing: border-box; position: relative; white-space: nowrap; height: auto; top: 5px;"><svg viewBox="0 0 24 24" style="display: inline-block; fill: rgba(0, 0, 0, 0.87); height: 36px; width: 24px; user-select: none; transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms; margin-right: 24px; float: left; position: relative; bottom: 10px;"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg><div style="display: block; vertical-align: top; white-space: normal; padding-right: 0px; padding-left: 50px;"><span style="color: rgba(0, 0, 0, 0.87); display: block; font-size: 15px;"><div style="margin-top: -10px;">' + atc[nowServerDetail][this.info.code][i].UserName + '<div></div></div></span><span style="color: rgba(0, 0, 0, 0.54); display: block; font-size: 14px;">Controller</span></div></div>';
+                }
+                html += '<hr>';
+                document.querySelector('#component-airport-info-ok').innerHTML = html;
+                document.querySelector('#component-airport-info-ok').style.display = 'block';
+                document.querySelector('#component-airport-info-error').style.display = 'none';
+            }
+
+            //显示面板
+            setTimeout(function() {
+                componentAirport.classList.add('show');
+                componentAirportCloseBar.classList.add('show-component-airport-closeBar')
+            }, 0);
         }
 
         mk.info = {
@@ -31,13 +104,8 @@ function getAirportExcelFn() {
             code: thisData[0],
             lat: thisData[2],
             lon: thisData[3],
-            ft: thisData[4],
-            isOpen: false
+            ft: thisData[4]
         }
-
-        mk.addEventListener('infowindowclose', function() {
-            this.info.isOpen = false;
-        })
 
         markersObj[thisData[0]] = mk;
         map.addOverlay(mk);
