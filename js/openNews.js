8221f9c6d5202b34b4ed47412fb51e59d35b7b98
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-08-06 21:05:32 +0800

    Update openNews.js

--- js/openNews.js
+++ js/openNews.js
@@ -1,4 +1,4 @@
-if (!localStorage.isClickedNews) {
+if (!localStorage.isClickedNews || !localStorage.newsExpireTime || (new Date().getTime() > (parseInt(localStorage.newsExpireTime) + (1000 * 60 * 60 * 24 * 10)))) {
     if (/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent)) || /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
         //手机访问
         layui.use('layer', function() {
@@ -11,6 +11,7 @@ if (!localStorage.isClickedNews) {
                 area: ['100%', '400px'],
                 content: 'news/news.html',
                 end: function() {
+                    localStorage.newsExpireTime = new Date().getTime().toString();
                     localStorage.isClickedNews = true;
                 }
             })
@@ -27,6 +28,7 @@ if (!localStorage.isClickedNews) {
                 area: ['693px', '400px'],
                 content: 'news/news.html',
                 end: function() {
+                    localStorage.newsExpireTime = new Date().getTime().toString();
                     localStorage.isClickedNews = true;
                 }
             })
