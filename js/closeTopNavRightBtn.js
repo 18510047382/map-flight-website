0a9112cb38b4e4fc8778102ec320de71428b1b25
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-08-10 11:50:28 +0800

    Delete closeTopNavRightBtn.js

--- js/closeTopNavRightBtn.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#top-nav-me-close-btn').onclick = function() {
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent('mouseout', true, true);
-    document.querySelector('#top-nav-me').dispatchEvent(evt);
-}
