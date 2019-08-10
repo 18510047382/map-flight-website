379e04680a047799db7860c25d7966a0b4d2faf8
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-08-10 11:51:51 +0800

    Update handleComponentAirport.js

--- js/handleComponentAirport.js
+++ js/handleComponentAirport.js
@@ -8,20 +8,35 @@ document.querySelector('#component-airport-closeBar').onclick = function() {
 }
 
 var comAirportMenuIndexBtn = document.querySelector('#component-airport-menu-indexBtn'),
+    comAirportMenuWeatherBtn = document.querySelector('#component-airport-menu-weatherBtn'),
     comAirportMenuInfoBtn = document.querySelector('#component-airport-menu-infoBtn'),
     comAirportIndex = document.querySelector('#component-airport-index'),
+    comAirportWeather = document.querySelector('#component-airport-weather'),
     comAirportInfo = document.querySelector('#component-airport-info');
 
 comAirportMenuIndexBtn.onclick = function() {
+    comAirportMenuWeatherBtn.classList.remove('component-airport-menu-act');
     comAirportMenuInfoBtn.classList.remove('component-airport-menu-act');
     this.classList.add('component-airport-menu-act');
+    comAirportWeather.style.display = 'none';
     comAirportInfo.style.display = 'none';
     comAirportIndex.style.display = 'block';
 }
 
 comAirportMenuInfoBtn.onclick = function() {
     comAirportMenuIndexBtn.classList.remove('component-airport-menu-act');
+    comAirportMenuWeatherBtn.classList.remove('component-airport-menu-act');
     this.classList.add('component-airport-menu-act');
     comAirportIndex.style.display = 'none';
+    comAirportWeather.style.display = 'none';
     comAirportInfo.style.display = 'block';
+}
+
+comAirportMenuWeatherBtn.onclick = function(){
+    comAirportMenuIndexBtn.classList.remove('component-airport-menu-act');
+    comAirportMenuInfoBtn.classList.remove('component-airport-menu-act');
+    this.classList.add('component-airport-menu-act');
+    comAirportIndex.style.display = 'none';
+    comAirportInfo.style.display = 'none';
+    comAirportWeather.style.display = 'block';
 }
