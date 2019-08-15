e123773b21d892c4f3e178a55b137fdc3326cfc8
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-08-15 12:02:17 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,14 +1,14 @@
 document.querySelector('#search-btn').onclick = function() {
     layer.alert('请选择搜索类型：', {
         title: '搜索',
-        btn: ['地点搜索', '航班搜索'],
+        btn: ['机场搜索', '航班搜索'],
         btn2: function(index) {
             layer.close(index);
             searchFlight();
         }
     }, function(index) {
         layer.close(index);
-        searchPlace();
+        searchAirport();
     })
 }
 
@@ -34,42 +34,26 @@ function searchFlight() {
     })
 }
 
-function searchPlace() {
+function searchAirport() {
     layer.prompt({
         formType: 0,
-        value: '北京首都机场',
-        title: '请输入地点（不支持境外搜索）',
+        title: '请输入机场四字码',
         area: ['800px', '350px']
     }, function(value, index, elem) {
-        var loadingPlaceLayer = layer.load(2);
-
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                layer.close(loadingPlaceLayer);
-                toPlace(JSON.parse(xmlhttp.responseText));
-            }
-        }
-        xmlhttp.open("GET", 'https://' + backendLink + ":8000/searchPlace?place=" + value, true);
-        xmlhttp.send();
-
         layer.close(index);
+
+        value = value.toUpperCase();
+
+        if (typeof airportMarkers[value] === 'undefined') {
+            layer.msg('没有此机场！');
+            return;
+        }
+
+        map.panTo(airportMarkers[value].getPosition(), {
+            noAnimation: true
+        })
+
+        getBoundsPlane();
+        getBoundsAirport();
     })
-}
-
-function toPlace(placeArr) {
-    placeArr = placeArr.result[0];
-
-    if (placeArr === undefined) {
-        layer.msg('没有此地点！');
-        return;
-    }
-
-    map.setZoom(12);
-    map.panTo(new BMap.Point(placeArr.location.lng, placeArr.location.lat), {
-        noAnimation: true
-    })
-
-    getBoundsPlane();
-    getBoundsAirport();
 }
