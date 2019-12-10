08473f29ef745c3f12bae228d6794b8b141d16f3
董英轩 <dongyingxuan@dongyingxuandeMacBook-Pro.local>
2019-12-10 15:59:23 +0800

    a350 update

---
+++ A350.js
@@ -0,0 +1,9 @@
+(function() {
+    document.querySelector('#top-nav-me').onclick = function() {
+        layer.alert('A350!!!!!', {
+            skin: 'layui-layer-lan',
+            closeBtn: 0,
+            anim: 4
+        })
+    }
+}());
