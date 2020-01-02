0cc71f2c3841194398378ddcba7719a7e25bb2b6
董英轩 <dongyingxuan@dongyingxuandeMacBook-Pro.local>
2020-01-02 08:27:35 +0800

    ads update

--- A350.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    document.querySelector('#top-nav-me').onclick = function() {
-        layer.alert('A350!!!!!', {
-            skin: 'layui-layer-lan',
-            closeBtn: 0,
-            anim: 4
-        })
-    }
-}());
---
+++ js/ads.js
@@ -0,0 +1,5 @@
+document.querySelector('#ads-img').onclick = function() {
+    layer.alert('扫描二维码加入QQ群：991489400<br><img src="img/ads/qrcode.jpg">', {
+        title: '欢迎加入IFMUVA！'
+    })
+}
